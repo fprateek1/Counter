dfe1892e9098138e6426953e09ed9a4004002559
fprateek1 <id.for.prateek@gmail.com>
2022-01-22 17:35:46 +0530

    new file

---
+++ app.js
@@ -0,0 +1,41 @@
+// set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+     const styles =e.currentTarget.classList;
+     if(styles.contains('decrease')){
+         count --;
+        //  document.getElementById('value').style.color='red'
+     }
+     else if(styles.contains('increase')){
+         count ++;
+        //  document.getElementById('value').style.color='green'
+
+     }
+     else
+      {
+         count = 0;
+         /*if(count == 0)
+            {
+               //
+            } */
+      }
+     if(count > 0){
+        value.style.color='green'
+     }
+     else if(count < 0){
+     value.style.color='red'
+     }
+     else{
+        value.style.color='black'
+
+     }
+     value.textContent = count;
+    });
+
+});
